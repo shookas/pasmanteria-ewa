11417903f52844ffd1656363a23481bd2f583bc1
pszukala <pszukala@pl.sii.eu>
2018-05-23 15:45:25 +0200

    slit into components

---
+++ src/Carousel/Carousel.js
@@ -0,0 +1,154 @@
+import React, { Component } from 'react';
+import './Carousel.scss';
+
+class Carousel extends Component {
+  render() {
+    return (
+      <section className="carousel">
+        <div className="reel">
+          <article>
+            <a href="#" className="image featured">
+              <img src="images/pic01.jpg" alt="" />
+            </a>
+            <header>
+              <h3>
+                <a href="#">Pulvinar sagittis congue</a>
+              </h3>
+            </header>
+            <p>
+              Commodo id natoque malesuada sollicitudin elit suscipit magna.
+            </p>
+          </article>
+
+          <article>
+            <a href="#" className="image featured">
+              <img src="images/pic02.jpg" alt="" />
+            </a>
+            <header>
+              <h3>
+                <a href="#">Fermentum sagittis proin</a>
+              </h3>
+            </header>
+            <p>
+              Commodo id natoque malesuada sollicitudin elit suscipit magna.
+            </p>
+          </article>
+
+          <article>
+            <a href="#" className="image featured">
+              <img src="images/pic03.jpg" alt="" />
+            </a>
+            <header>
+              <h3>
+                <a href="#">Sed quis rhoncus placerat</a>
+              </h3>
+            </header>
+            <p>
+              Commodo id natoque malesuada sollicitudin elit suscipit magna.
+            </p>
+          </article>
+
+          <article>
+            <a href="#" className="image featured">
+              <img src="images/pic04.jpg" alt="" />
+            </a>
+            <header>
+              <h3>
+                <a href="#">Ultrices urna sit lobortis</a>
+              </h3>
+            </header>
+            <p>
+              Commodo id natoque malesuada sollicitudin elit suscipit magna.
+            </p>
+          </article>
+
+          <article>
+            <a href="#" className="image featured">
+              <img src="images/pic05.jpg" alt="" />
+            </a>
+            <header>
+              <h3>
+                <a href="#">Varius magnis sollicitudin</a>
+              </h3>
+            </header>
+            <p>
+              Commodo id natoque malesuada sollicitudin elit suscipit magna.
+            </p>
+          </article>
+
+          <article>
+            <a href="#" className="image featured">
+              <img src="images/pic01.jpg" alt="" />
+            </a>
+            <header>
+              <h3>
+                <a href="#">Pulvinar sagittis congue</a>
+              </h3>
+            </header>
+            <p>
+              Commodo id natoque malesuada sollicitudin elit suscipit magna.
+            </p>
+          </article>
+
+          <article>
+            <a href="#" className="image featured">
+              <img src="images/pic02.jpg" alt="" />
+            </a>
+            <header>
+              <h3>
+                <a href="#">Fermentum sagittis proin</a>
+              </h3>
+            </header>
+            <p>
+              Commodo id natoque malesuada sollicitudin elit suscipit magna.
+            </p>
+          </article>
+
+          <article>
+            <a href="#" className="image featured">
+              <img src="images/pic03.jpg" alt="" />
+            </a>
+            <header>
+              <h3>
+                <a href="#">Sed quis rhoncus placerat</a>
+              </h3>
+            </header>
+            <p>
+              Commodo id natoque malesuada sollicitudin elit suscipit magna.
+            </p>
+          </article>
+
+          <article>
+            <a href="#" className="image featured">
+              <img src="images/pic04.jpg" alt="" />
+            </a>
+            <header>
+              <h3>
+                <a href="#">Ultrices urna sit lobortis</a>
+              </h3>
+            </header>
+            <p>
+              Commodo id natoque malesuada sollicitudin elit suscipit magna.
+            </p>
+          </article>
+
+          <article>
+            <a href="#" className="image featured">
+              <img src="images/pic05.jpg" alt="" />
+            </a>
+            <header>
+              <h3>
+                <a href="#">Varius magnis sollicitudin</a>
+              </h3>
+            </header>
+            <p>
+              Commodo id natoque malesuada sollicitudin elit suscipit magna.
+            </p>
+          </article>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Carousel;
---
+++ src/Footer/Footer.js
@@ -0,0 +1,200 @@
+import React, { Component } from 'react';
+import './Footer.scss';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div id="footer">
+        <div className="container">
+          <div className="row">
+            <section className="4u 12u(mobile)">
+              <header>
+                <h2 className="icon fa-twitter circled">
+                  <span className="label">Tweets</span>
+                </h2>
+              </header>
+              <ul className="divided">
+                <li>
+                  <article className="tweet">
+                    Amet nullam fringilla nibh nulla convallis tique ante sociis
+                    accumsan.
+                    <span className="timestamp">5 minutes ago</span>
+                  </article>
+                </li>
+                <li>
+                  <article className="tweet">
+                    Hendrerit rutrum quisque.
+                    <span className="timestamp">30 minutes ago</span>
+                  </article>
+                </li>
+                <li>
+                  <article className="tweet">
+                    Curabitur donec nulla massa laoreet nibh. Lorem praesent
+                    montes.
+                    <span className="timestamp">3 hours ago</span>
+                  </article>
+                </li>
+                <li>
+                  <article className="tweet">
+                    Lacus natoque cras rhoncus curae dignissim ultricies.
+                    Convallis orci aliquet.
+                    <span className="timestamp">5 hours ago</span>
+                  </article>
+                </li>
+              </ul>
+            </section>
+            {/* // Posts */}
+            <section className="4u 12u(mobile)">
+              <header>
+                <h2 className="icon fa-file circled">
+                  <span className="label">Posts</span>
+                </h2>
+              </header>
+              <ul className="divided">
+                <li>
+                  <article className="post stub">
+                    <header>
+                      <h3>
+                        <a href="#">Nisl fermentum integer</a>
+                      </h3>
+                    </header>
+                    <span className="timestamp">3 hours ago</span>
+                  </article>
+                </li>
+                <li>
+                  <article className="post stub">
+                    <header>
+                      <h3>
+                        <a href="#">Phasellus portitor lorem</a>
+                      </h3>
+                    </header>
+                    <span className="timestamp">6 hours ago</span>
+                  </article>
+                </li>
+                <li>
+                  <article className="post stub">
+                    <header>
+                      <h3>
+                        <a href="#">Magna tempus consequat</a>
+                      </h3>
+                    </header>
+                    <span className="timestamp">Yesterday</span>
+                  </article>
+                </li>
+                <li>
+                  <article className="post stub">
+                    <header>
+                      <h3>
+                        <a href="#">Feugiat lorem ipsum</a>
+                      </h3>
+                    </header>
+                    <span className="timestamp">2 days ago</span>
+                  </article>
+                </li>
+              </ul>
+            </section>
+            {/* // Photos */}
+            <section className="4u 12u(mobile)">
+              <header>
+                <h2 className="icon fa-camera circled">
+                  <span className="label">Photos</span>
+                </h2>
+              </header>
+              <div className="row 25%">
+                <div className="6u">
+                  <a href="#" className="image fit">
+                    <img src="images/pic10.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="6u">
+                  <a href="#" className="image fit">
+                    <img src="images/pic11.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="6u">
+                  <a href="#" className="image fit">
+                    <img src="images/pic12.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="6u">
+                  <a href="#" className="image fit">
+                    <img src="images/pic13.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="6u">
+                  <a href="#" className="image fit">
+                    <img src="images/pic14.jpg" alt="" />
+                  </a>
+                </div>
+                <div className="6u">
+                  <a href="#" className="image fit">
+                    <img src="images/pic15.jpg" alt="" />
+                  </a>
+                </div>
+              </div>
+            </section>
+          </div>
+          <hr />
+          <div className="row">
+            <div className="12u">
+              {/* // Contact */}
+              <section className="contact">
+                <header>
+                  <h3>Nisl turpis nascetur interdum?</h3>
+                </header>
+                <p>
+                  Urna nisl non quis interdum mus ornare ridiculus egestas
+                  ridiculus lobortis vivamus tempor aliquet.
+                </p>
+                <ul className="icons">
+                  <li>
+                    <a href="#" className="icon fa-twitter">
+                      <span className="label">Twitter</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="icon fa-facebook">
+                      <span className="label">Facebook</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="icon fa-instagram">
+                      <span className="label">Instagram</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="icon fa-pinterest">
+                      <span className="label">Pinterest</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="icon fa-dribbble">
+                      <span className="label">Dribbble</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="icon fa-linkedin">
+                      <span className="label">Linkedin</span>
+                    </a>
+                  </li>
+                </ul>
+              </section>
+
+              {/* // Copyright */}
+              <div className="copyright">
+                <ul className="menu">
+                  <li>&copy; Untitled. All rights reserved.</li>
+                  <li>
+                    Design: <a href="http://html5up.net">HTML5 UP</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
